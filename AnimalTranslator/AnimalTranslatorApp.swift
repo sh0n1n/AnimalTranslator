54bd13e67a53ec0df3eff889b4d326cb31172373
Artem <dogmaster31072003@gmail.com>
2025-03-01 20:11:12 +0200

    Initial Commit

---
+++ AnimalTranslator/AnimalTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalTranslatorApp.swift
+//  AnimalTranslator
+//
+//  Created by Melkor on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimalTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalTranslatorTests/AnimalTranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalTranslatorTests.swift
+//  AnimalTranslatorTests
+//
+//  Created by Melkor on 3/1/25.
+//
+
+import Testing
+@testable import AnimalTranslator
+
+struct AnimalTranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
